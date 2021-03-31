4b4c9186aec0028f9769c51ad5adb225b9b4936a
Wang Guosheng <wguosh@gmail.com>
2021-04-01 01:25:26 +0800

    edit include

--- codehl.py
+++ codehl.py
@@ -3,6 +3,27 @@ from pygments.lexers import RubyLexer
 from pygments.formatters import HtmlFormatter
 
 import os
+import datetime
+import random
+
+
+def get_time_string():
+    d = datetime.datetime.now()
+    return d.strftime('%Y_%m_%d_%H_%M_')
+
+
+def print_inlcude(dir_fname):
+    result = dict()
+    tstring = get_time_string() + hex(random.randrange(16**8))[2:]
+    result['div'] = '<div id = "includedContent_' + tstring + '"></div>'
+
+    with open("template.js") as jstemp:
+        js_content = ''.join(jstemp.readlines())
+    base = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/"
+    url = base + dir_fname.replace('\\', '/')
+    result['js'] = js_content.replace('TSTRING', tstring).replace('URL', url)
+
+    return result
 
 
 def ruby2html(ruby_file):
@@ -15,13 +36,18 @@ def ruby2html(ruby_file):
 
 
 def convert_dir(directory, operation):
+    results = []
     for filename in os.listdir(directory):
         if filename.endswith(".rb"):
-            fullpath = ''.join([directory, '\\', filename])
-            operation(fullpath)
+            dir_fname = ''.join([directory, '\\', filename])
+            results.append(operation(dir_fname))
             continue
         else:
             continue
+    return results
 
 
-convert_dir('day1', ruby2html)
+# convert_dir('day1', ruby2html)
+results = convert_dir('day1', print_inlcude)
+print('\n'.join([res['div'] for res in results]))
+print('\n'.join([res['js'] for res in results]))
---
+++ template.js
@@ -0,0 +1,4 @@
+$(function(){
+    var url_TSTRING = "URL";
+    $("#includedContent_TSTRING").load(url_TSTRING)
+})
